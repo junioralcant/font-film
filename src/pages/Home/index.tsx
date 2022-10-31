8eacbad243d85703252413928b2ab31bd57dfb82
junioralcant <junioralcant111@gmail.com>
2022-10-31 10:58:51 -0300

    import component film in page home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,36 @@
-import { Container } from './styles';
+import { Film } from '../../components/Film';
+import { Container, ContainerFilms } from './styles';
 
 export function Home() {
   return (
     <Container>
-      <h1>Home</h1>
+      <ContainerFilms>
+        <Film
+          background="https://image.tmdb.org/t/p/original/xtPBZYaWQMQxRpy7mkdk5n1bTxs.jpg"
+          title="Tropa de Elite 2"
+          description="Nascimento é afastado do Bope após uma operação que dá errado e vai para a Secretaria de Segurança Pública do Rio."
+        />
+        <Film
+          background="https://image.tmdb.org/t/p/original/xtPBZYaWQMQxRpy7mkdk5n1bTxs.jpg"
+          title="Tropa de Elite 2"
+          description="Nascimento é afastado do Bope após uma operação que dá errado e vai para a Secretaria de Segurança Pública do Rio."
+        />
+        <Film
+          background="https://image.tmdb.org/t/p/original/xtPBZYaWQMQxRpy7mkdk5n1bTxs.jpg"
+          title="Tropa de Elite 2"
+          description="Nascimento é afastado do Bope após uma operação que dá errado e vai para a Secretaria de Segurança Pública do Rio."
+        />
+        <Film
+          background="https://image.tmdb.org/t/p/original/xtPBZYaWQMQxRpy7mkdk5n1bTxs.jpg"
+          title="Tropa de Elite 2"
+          description="Nascimento é afastado do Bope após uma operação que dá errado e vai para a Secretaria de Segurança Pública do Rio."
+        />
+        <Film
+          background="https://image.tmdb.org/t/p/original/xtPBZYaWQMQxRpy7mkdk5n1bTxs.jpg"
+          title="Tropa de Elite 2"
+          description="Nascimento é afastado do Bope após uma operação que dá errado e vai para a Secretaria de Segurança Pública do Rio."
+        />
+      </ContainerFilms>
     </Container>
   );
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,17 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.main`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ContainerFilms = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 2rem;
+
+  width: 1000px;
+
+  margin-top: 3rem;
+`;
