69febeeb7ab42a2f690342541afe693d6d9fc4f0
junioralcant <junioralcant111@gmail.com>
2022-10-31 09:34:25 -0300

    config themes styles and routes in file app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from './pages/styles/global';
+import { Router } from './Routes';
+import { defaultTheme } from './pages/styles/themes/default';
+
 function App() {
-  return <h1>Create project</h1>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 }
 
 export default App;
